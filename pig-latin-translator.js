b13d1db2c38fa23f5af70e348a931462cfb1804d
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-02-20 15:33:48 -0800

    solve pig latin kata

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -1,16 +1,13 @@
-// const args = process.argv;
-
 const toPigLatin = (input) => {
   const pigLatin = [];
   let words = input.slice(2);
 
   for (const word of words) {
-    console.log("log", word.slice(1) + word[0] + "ay");
+    pigLatin.push(word.slice(1) + word[0] + "ay");
+    // console.log("log", word.slice(1) + word[0] + "ay");
   }
 
-  return pigLatin;
+  return pigLatin.join(" ");
 };
 
-// console.log(toPigLatin(process.argv));
-
-console.log(toPigLatin([0, 0, "snail", "hole"]));
+console.log(toPigLatin(process.argv));
