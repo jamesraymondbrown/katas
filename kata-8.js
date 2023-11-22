a050015501e058d95acfb39b06ae1a100182cd35
James Brown <jamesraymondbrown@gmail.com>
2023-11-21 18:30:38 -0800

    add question for kata-8

---
+++ kata-8.js
@@ -0,0 +1,18 @@
+const repeatNumbers = function (data) {
+  // Put your solution here
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+);
+console.log(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+);
