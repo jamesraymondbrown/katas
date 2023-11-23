e02f48bc09bdb37725cb161c8aa02993534f2a84
James Brown <jamesraymondbrown@gmail.com>
2023-11-22 20:57:20 -0800

    solve kata-11

--- kata-11.js
+++ kata-11.js
@@ -2,25 +2,37 @@
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return (4 / 3) * PI * (radius * radius * radius);
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
 const coneVolume = function (radius, height) {
-  // And here!
+  return PI * (radius * radius) * (height / 3);
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return height * width * depth;
 };
 
 console.log(prismVolume(3, 4, 5) === 60);
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  let volume = 0;
+
+  solids.forEach((solid) => {
+    if (solid.type === "sphere") {
+      volume += sphereVolume(solid.radius);
+    } else if (solid.type === "cone") {
+      volume += coneVolume(solid.radius, solid.height);
+    } else if (solid.type === "prism") {
+      volume += prismVolume(solid.base, solid.width, solid.height);
+    }
+  });
+
+  return volume;
 };
 
 const largeSphere = {
