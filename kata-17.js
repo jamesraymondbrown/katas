2ad37239dbeeeda83eb3ac5b89721fe75aa7286a
James Brown <jamesraymondbrown@gmail.com>
2023-11-27 13:28:39 -0800

    solve kata 17

---
+++ kata-17.js
@@ -0,0 +1,17 @@
+const urlDecode = function (text) {
+  const urlObject = {};
+
+  const keyValuePairs = text.split("&");
+
+  for (let pair of keyValuePairs) {
+    const splitPair = pair.split("=");
+    urlObject[splitPair[0]] = splitPair[1].split("%20").join(" ");
+  }
+
+  return urlObject;
+};
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
